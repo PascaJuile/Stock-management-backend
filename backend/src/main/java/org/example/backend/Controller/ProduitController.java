7b87fed94e9110331fb2221beef772581ddc28c8
PascaJuile <pascalinejuilepascajuile@gmail.com>
2025-06-03 20:05:26 +0000

    requête PUT de modification de produit

--- backend/src/main/java/org/example/backend/Controller/ProduitController.java
+++ backend/src/main/java/org/example/backend/Controller/ProduitController.java
@@ -2,10 +2,13 @@ package org.example.backend.Controller;
 
 import org.example.backend.Dto.ProduitRequest;
 import org.example.backend.Dto.ProduitResponse;
+import org.example.backend.Entity.Produit;
 import org.example.backend.Service.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/produits")
@@ -19,5 +22,12 @@ public class ProduitController {
     public ResponseEntity<ProduitResponse> createProduit(@RequestBody ProduitRequest produitRequest){
         ProduitResponse produitResponse = produitService.createProduit(produitRequest);
         return ResponseEntity.ok(produitResponse);
+    }
+
+    @PutMapping("update-produit/{trackingId}")
+    public ResponseEntity<ProduitResponse> updateProduit(@RequestBody ProduitRequest produitRequest, @PathVariable UUID trackingId){
+        ProduitResponse produitResponse = produitService.updateProduit(produitRequest, trackingId);
+        return ResponseEntity.ok(produitResponse);
+
     }
 }
